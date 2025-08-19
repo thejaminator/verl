8601fe68bbd1c6ad8911a35ef23febf5fcff8ba1
James Chua <chuajamessh@gmail.com>
2025-08-20 04:11:35 +0800

    add

--- create_hard_negative_and_feature_vector.py
+++ create_hard_negative_and_feature_vector.py
@@ -778,6 +778,6 @@ def main(
 if __name__ == "__main__":
     # Example usage - customize the feature_idxs and other parameters as needed
     # first 500 features
-    target_features = list(range(2_000))
+    target_features = list(range(11_000))
     # actually we want 32, but sometimes it fails, so need some buffer.
     main(target_features=target_features, top_k_similar_features=34, batch_size=1024, target_sentences=32)
